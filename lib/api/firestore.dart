a2141aa402ba683f360e3ec821dbaf0587a096c4
kip <kkipngenokoech22@gmail.com>
2023-08-27 18:36:24 +0300

    sign up auth complete time to build the sign in

--- lib/api/firestore.dart
+++ lib/api/firestore.dart
@@ -1,38 +1,52 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:flutterbook/notes/notes_model.dart';
 
 class FirestoreService {
   static Future<List<NoteModel>> fetchNotes() async {
-    QuerySnapshot querySnapshot = await FirebaseFirestore.instance.collection("notes").get();
-    return querySnapshot.docs.map((doc) => NoteModel.fromMap(doc.id, doc.data() as Map<String, dynamic>)).toList();
+    QuerySnapshot querySnapshot =
+        await FirebaseFirestore.instance.collection("notes").get();
+    return querySnapshot.docs
+        .map((doc) =>
+            NoteModel.fromMap(doc.id, doc.data() as Map<String, dynamic>))
+        .toList();
   }
 
-  final CollectionReference _notesCollection = FirebaseFirestore.instance.collection("notes");
+  final CollectionReference _notesCollection =
+      FirebaseFirestore.instance.collection("notes");
 
   Future<void> addData(String title, String content, Color color) async {
     try {
-      await _notesCollection.add({
-        'title': title,
-        'content': content,
-        'color': color.toString()
-      });
-      
+      await _notesCollection
+          .add({'title': title, 'content': content, 'color': color.toString()});
     } catch (error) {
-      throw FirebaseException(message: "Error adding data to firestore: $error", plugin: '');
+      throw FirebaseException(
+          message: "Error adding data to firestore: $error", plugin: '');
     }
   }
 
   static Future<void> updateNote(NoteModel note) async {
-    final noteUpdate = FirebaseFirestore.instance.collection('notes').doc(note.id);
-    await noteUpdate.update({
-      'title': note.title,
-      'content': note.content
-    });
+    final noteUpdate =
+        FirebaseFirestore.instance.collection('notes').doc(note.id);
+    await noteUpdate.update({'title': note.title, 'content': note.content});
   }
 
   static Future<void> deleteNode(String note) async {
-    final CollectionReference notesCollections = FirebaseFirestore.instance.collection('notes');
+    final CollectionReference notesCollections =
+        FirebaseFirestore.instance.collection('notes');
     await notesCollections.doc(note).delete();
+  }
+
+  // sign up with firebase auth.
+  Future<Object?> signUpWithEmailAndPassword(String email, String password) async {
+    try {
+      UserCredential userCredential = await FirebaseAuth.instance
+          .createUserWithEmailAndPassword(email: email, password: password);
+      User? user = userCredential.user;
+      return user;
+    } catch (error) {
+      return "$error";
+    }
   }
 }
--- lib/auth/signup.dart
+++ lib/auth/signup.dart
@@ -1,5 +1,7 @@
 import 'package:email_validator/email_validator.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
+import 'package:flutterbook/api/firestore.dart';
 
 class SignupWidgetSceen extends StatefulWidget {
   const SignupWidgetSceen({super.key});
@@ -14,19 +16,26 @@ class _SignupWidgetSceenState extends State<SignupWidgetSceen> {
   final TextEditingController _phoneController = TextEditingController();
   final TextEditingController _emailController = TextEditingController();
   final TextEditingController _passwordController = TextEditingController();
-  final TextEditingController _passwordConfirmController = TextEditingController();
+  final TextEditingController _passwordConfirmController =
+      TextEditingController();
 
-  void _submitForm() {
+  Future<void> _submitForm(BuildContext context) async {
     if (_formKey.currentState!.validate()) {
       print("Email: ${_emailController.text}");
       print("Password: ${_passwordController.text}");
+      var results = await FirestoreService().signUpWithEmailAndPassword(
+          _emailController.text, _passwordController.text);
+      if (results is User) {
+        Navigator.pushNamed(context, '/');
+      } else if (results is String){
+        ScaffoldMessenger.of(context).showSnackBar(SnackBar(content: Text(results)));
+      }
     }
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-
       body: Container(
         width: double.infinity,
         height: MediaQuery.of(context).size.height,
@@ -36,16 +45,15 @@ class _SignupWidgetSceenState extends State<SignupWidgetSceen> {
             child: Column(
               mainAxisAlignment: MainAxisAlignment.center,
               children: [
-                TextFormField( 
+                TextFormField(
                   controller: _nameController,
-                  decoration:  InputDecoration(
-                    labelText: "full name",
-                    border: OutlineInputBorder(
-                      borderRadius: BorderRadius.circular(10.0),
-                    )
-                  ),
-                  validator: (value){
-                    if (value == null || value.isEmpty){
+                  decoration: InputDecoration(
+                      labelText: "full name",
+                      border: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(10.0),
+                      )),
+                  validator: (value) {
+                    if (value == null || value.isEmpty) {
                       return 'please enter your name';
                     }
                     return null;
@@ -53,37 +61,32 @@ class _SignupWidgetSceenState extends State<SignupWidgetSceen> {
                 ),
                 const SizedBox(height: 20),
                 TextFormField(
-                  controller:_phoneController,
-                  keyboardType: TextInputType.phone,
-                  decoration: InputDecoration(
-                    labelText: "phone number",
-                    border: OutlineInputBorder(
-                      borderRadius: BorderRadius.circular(10.0)
-                    )
-                  ),
-                  validator: (value){
-                    if(value == null || value.isEmpty){
-                      return "please enter your phone number";
-                    }
-                    return null;
-                  }
-                ),
+                    controller: _phoneController,
+                    keyboardType: TextInputType.phone,
+                    decoration: InputDecoration(
+                        labelText: "phone number",
+                        border: OutlineInputBorder(
+                            borderRadius: BorderRadius.circular(10.0))),
+                    validator: (value) {
+                      if (value == null || value.isEmpty) {
+                        return "please enter your phone number";
+                      }
+                      return null;
+                    }),
                 const SizedBox(height: 20),
                 TextFormField(
                   controller: _emailController,
                   keyboardType: TextInputType.emailAddress,
                   decoration: InputDecoration(
-                    labelText: 'email',
-                    border: OutlineInputBorder(
-                      borderRadius: BorderRadius.circular(10.0)
-                    )
-                  ),
+                      labelText: 'email',
+                      border: OutlineInputBorder(
+                          borderRadius: BorderRadius.circular(10.0))),
                   validator: (value) {
                     if (value == null || value.isEmpty) {
                       return 'please enter your email';
                     }
                     if (!EmailValidator.validate(value)) {
-                      return 'please enter a valide email';
+                      return 'please enter a valid email';
                     }
                     return null;
                   },
@@ -92,12 +95,10 @@ class _SignupWidgetSceenState extends State<SignupWidgetSceen> {
                 TextFormField(
                   controller: _passwordController,
                   obscureText: true,
-                  decoration:  InputDecoration(
-                    labelText: "password",
-                    border: OutlineInputBorder(
-                      borderRadius: BorderRadius.circular(10.0)
-                    )
-                  ),
+                  decoration: InputDecoration(
+                      labelText: "password",
+                      border: OutlineInputBorder(
+                          borderRadius: BorderRadius.circular(10.0))),
                   validator: (value) {
                     if (value == null || value.isEmpty) {
                       return 'please enter your password';
@@ -109,11 +110,10 @@ class _SignupWidgetSceenState extends State<SignupWidgetSceen> {
                 TextFormField(
                   controller: _passwordConfirmController,
                   obscureText: true,
-                  decoration:  InputDecoration(
+                  decoration: InputDecoration(
                     labelText: "confirm password",
                     border: OutlineInputBorder(
-                      borderRadius: BorderRadius.circular(10.0)
-                    ),
+                        borderRadius: BorderRadius.circular(10.0)),
                   ),
                   validator: (value) {
                     if (value == null || value.isEmpty) {
@@ -127,15 +127,11 @@ class _SignupWidgetSceenState extends State<SignupWidgetSceen> {
                 ),
                 const SizedBox(height: 20),
                 ElevatedButton(
-                  onPressed: _submitForm,
-                  child: const Text("sign up")
-                ),
+                    onPressed: () =>  _submitForm(context), child: const Text("sign up")),
                 const Center(
                   child: Text("Already have an account?"),
                 )
-
               ],
-            
             )),
       ),
     );
